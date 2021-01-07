386dd62abc9072340da6354d174f6c5bdddb08a7
khalid raoufi <khalidraoufi@yahoo.fr>
2021-01-07 16:28:34 +0100

    commit test Cteo-Sodifrance-KRA

--- TestEntretien/TestAlgo.cs
+++ TestEntretien/TestAlgo.cs
@@ -31,9 +31,49 @@ namespace TestEntretien
         public static void GroupAnagram()
         {
             Console.WriteLine("GroupAnagram start");
-            string[] input = { "reza","eat", "tea", "tan", "ate", "nat", "bat", "bta", "azer" };
+            string[] input = { "reza","eat", "tea", "tan", "ate", "nate", "bat", "bta", "azer" };
 
-           
+            List<string> anagram = new List<string>();
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (i != j)
+                    {
+                        if (IsAnagram(input[j], input[i]))
+                        {
+                            anagram.Add(input[j] + "-" + input[i]);
+                        }
+                    }
+                    
+                }               
+                
+            }
+
+            Console.WriteLine(anagram.ToString());
+        }
+
+        private static bool IsAnagram(string text1, string text2)
+        {
+            var tabText1 = text1.ToCharArray();
+            var tabText2 = text2.ToCharArray();
+
+            if(tabText1.Length == tabText2.Length)
+            {
+                for (int i = 0; i < tabText1.Length; i++)
+                {
+                    if (!text2.Contains(tabText1[i]))
+                    {
+                        return false;
+                    }                    
+                }
+
+            } else
+            {
+                return false;
+            }
+            return true;
         }
 
        
--- TestEntretien/TestLinq.cs
+++ TestEntretien/TestLinq.cs
@@ -14,6 +14,7 @@ namespace TestEntretien
                 "aabccccc",
                 "ddflfccccc"
             };
+            
                     
         }
         public static void MergeIndexAndRemoveDuplicate()
